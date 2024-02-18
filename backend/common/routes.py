4f202700e04478309664ca3200b2867c3c51aeec
Zubair Shaikh <zsescape@gmail.com>
2024-02-18 01:44:23 -0500

    Add recipe urls - Add recipe.views - Add recipe.routes

--- backend/common/routes.py
+++ backend/common/routes.py
@@ -1,6 +1,8 @@
 from .views import RestViewSet
-
+from recipe.views import RecipeViewSet, IngredientViewSet
 
 routes = [
     {"regex": r"rest", "viewset": RestViewSet, "basename": "Rest"},
+    {"regex": r"recipe", "viewset": RecipeViewSet, "basename": "Recipe"},
+    {"regex": r"ingredient", "viewset": IngredientViewSet, "basename": "Ingredient"},
 ]
--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
+from .serializers import IngredientSerializer, RecipeSerializer
 
-# Create your views here.
+from .models import Ingredient, Recipe
+
+
+class IngredientViewSet(ModelViewSet):
+    lookup_field = "id"
+    queryset = Ingredient.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = IngredientSerializer
+
+    def post(self, request):
+        try:
+            ingredient = Ingredient.objects.create(**request.data)
+            return Response(data=ingredient, status=status.HTTP_201_CREATED)
+        except ValidationError as errors:
+            return Response(data=errors.message_dict, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RecipeViewSet(ModelViewSet):
+    lookup_field = "id"
+    queryset = Recipe.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RecipeSerializer
+
+    def post(self, request):
+        try:
+            recipe = Recipe.objects.create(**request.data)
+            return Response(data=recipe, status=status)
+        except ValidationError as error:
+            return Response(data=error.message_dict, status=status.HTTP_400_BAD_REQUEST)
